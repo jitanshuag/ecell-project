307edc764c88e8047c06e969af129abfe6c44c17
jitanshu agrawal <jitanshuagrawal074@gmail.com>
2023-09-16 21:27:14 +0530

    much more improved working of grid squares

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-// square color change
 const squares = document.querySelectorAll(".square");
+let squaresClicked = [];
 
 squares.forEach((square) => {
     square.addEventListener("click", () => {
         if (square.style.backgroundColor === "rgb(32, 32, 144)") {
             square.style.backgroundColor = "red";
+            let index = squaresClicked.indexOf(square);
+            if (index > -1) {
+                squaresClicked.splice(index, 1);
+            }
+        } else {
+            square.style.backgroundColor = "red";
+            squaresClicked.push(square);
         }
     });
 });
 
-// this will Change all square colors to blue on clicking the last square
+
 squares[squares.length - 1].addEventListener("click", () => {
-    squares.forEach((square) => {
-        square.style.backgroundColor = "rgb(32, 32, 144)";
-    });
+    setTimeout(() => {
+        squaresClicked.forEach((ClickedOne, index) => {
+            setTimeout(() => {
+                ClickedOne.style.backgroundColor = "rgb(32, 32, 144)";
+            }, 500 * index);
+        });
+    }, 1000);
 });
